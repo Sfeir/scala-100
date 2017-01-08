18f9bf69f244432608201759397c53c86244b7b4
avandendaele <alexis.vandendaele@gmail.com>
2017-01-08 22:18:01 +0100

    Define isComingSoon in Training

--- src/main/scala/com/sfeir/school/scala/Training.scala
+++ src/main/scala/com/sfeir/school/scala/Training.scala
@@ -12,6 +12,13 @@ case class Training(topic: String, level: Int, planning: Seq[(Int, Location)]) {
 
   lazy val name: String =
     s"$topic-$level"
+
+  def isComingSoon(from: Location, to: Location): Boolean =
+    locations.dropWhile(location => location != from) match {
+      case `from` +: `to` +: _ => true
+      case `from` +: _ +: `to` +: _ => true
+      case _ => false
+    }
 }
 
 
