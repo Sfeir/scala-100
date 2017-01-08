34b04b381bbca2b239d147628a9f2024c96524a2
avandendaele <alexis.vandendaele@gmail.com>
2017-01-08 18:25:11 +0100

    Define locations member of Training

--- src/main/scala/com/sfeir/school/scala/Training.scala
+++ src/main/scala/com/sfeir/school/scala/Training.scala
@@ -3,8 +3,12 @@ package com.sfeir.school.scala
 /**
   * Sfeir School - Scala 100
   */
-case class Training(topic: String, level: Int, planning: Seq[Location]) {
+case class Training(topic: String, level: Int, planning: Seq[(Int, Location)]) {
   require(planning.nonEmpty, "planning must contains at least one element")
+
+  val locations: Seq[Location] =
+    // Could also be written in short notation: planning map (_._2)
+    planning.map(plan => plan._2)
 }
 
 
