5db3082d47ea2dcc9450b71838a80b65a794523f
avandendaele <alexis.vandendaele@gmail.com>
2017-01-08 20:02:51 +0100

    Define method trainingAt at SfeirSchool

--- src/main/scala/com/sfeir/school/scala/SfeirSchool.scala
+++ src/main/scala/com/sfeir/school/scala/SfeirSchool.scala
@@ -8,4 +8,8 @@ case class SfeirSchool(trainings: Set[Training]) {
   val locations: Set[Location] =
   // Could also be written in short notation: trainings flatMap (_.locations)
   trainings.flatMap(training => training.locations)
+
+  def trainingsAt(location: Location): Set[Training] =
+    // Could also be written in short notation: trainings.filter(_.locations contains location)
+    trainings.filter(training => training.locations contains location)
 }
