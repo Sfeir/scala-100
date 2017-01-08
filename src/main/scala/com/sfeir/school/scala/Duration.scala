c151b916bbe168eb01b7dac9a37aac99dd7c80f2
avandendaele <alexis.vandendaele@gmail.com>
2017-01-08 16:06:55 +0100

    Add requirement to Duration

--- src/main/scala/com/sfeir/school/scala/Duration.scala
+++ src/main/scala/com/sfeir/school/scala/Duration.scala
@@ -12,7 +12,7 @@ object Duration {
 }
 
 class Duration(val days: Int = 0, val hours: Int = 0) {
-  // TODO: Check that hours is between 0 and 8
+  require(hours >= 0 && hours <= 7, "Hours must be between 0 and 7")
 
   val asHours: Int =
     days * 8 + hours
