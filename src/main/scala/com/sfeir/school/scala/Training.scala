33695f8e38efa7b2ccd9689bcc5de49674d2e28f
avandendaele <alexis.vandendaele@gmail.com>
2017-01-08 18:09:29 +0100

    Define case class Location Add planning parameter to Training

--- src/main/scala/com/sfeir/school/scala/Training.scala
+++ src/main/scala/com/sfeir/school/scala/Training.scala
@@ -3,4 +3,9 @@ package com.sfeir.school.scala
 /**
   * Sfeir School - Scala 100
   */
-case class Training(topic: String, level: Int)
+case class Training(topic: String, level: Int, planning: Seq[Location]) {
+  require(planning.nonEmpty, "planning must contains at least one element")
+}
+
+
+case class Location(name: String)
