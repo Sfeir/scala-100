aad5b27fa2e38d2ee462965bcfde7c014ef05f18
avandendaele <alexis.vandendaele@gmail.com>
2017-01-08 16:00:54 +0100

    Add Duration's companion object

--- src/main/scala/com/sfeir/school/scala/Duration.scala
+++ src/main/scala/com/sfeir/school/scala/Duration.scala
@@ -3,6 +3,14 @@ package com.sfeir.school.scala
 /**
   * Sfeir School - Scala 100
   */
+
+object Duration {
+
+  def fromHours(hours: Int): Duration =
+    new Duration(hours / 8, hours % 8)
+
+}
+
 class Duration(val days: Int = 0, val hours: Int = 0) {
   // TODO: Check that hours is between 0 and 8
 
